8d9df1afbb4d0de29a55810afbe5afee0ed2eead
Lobahub <92989709+Lobahub@users.noreply.github.com>
2021-10-26 13:30:49 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace prueba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prac = 30;
+            int teo  = 60;
+            int part = 10;
+            int notFin;
+
+
+            Console.WriteLine(" ingrese la nota de la practica del alumno");
+            prac = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ingrese la nota de las teoria del alumno");
+            teo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ingrese la nota de la participacion del alumno");
+            part = int.Parse(Console.ReadLine());
+
+         
+            notFin = prac + teo + part;
+            Console.WriteLine("la nota final es de: "+ notFin);
+
+
+            Console.ReadKey();
+        }
+    }
+}
